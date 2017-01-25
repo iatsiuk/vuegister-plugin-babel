2c26e8cb356128e501a3df1ae162d8b0a47a29aa
Aleksei Iatsiuk <a.v.iatsiuk@gmail.com>
2017-01-26 01:28:22 +0400

    new test and api

--- test/test.js
+++ test/test.js
@@ -1,24 +1,30 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 const assert = require('chai').assert;
 const transform = require('../');
 
 describe('vuegister-plugin-babel', () => {
+  // helpers
+  let dir = __dirname + '/fixtures/';
   let file = (name) => {
-    return fs.readFileSync(__dirname + '/fixtures/' + name, 'utf8');
+    let data = fs.readFileSync(dir + name, 'utf8');
+
+    return path.extname(name) === '.json' ?
+           JSON.parse(data) :
+           data;
   };
 
   it('transform', () => {
-    let generated = transform(file('original.js'), {});
+    let generated = transform(file('original.js'), {}).code;
 
     assert.strictEqual(generated, file('generated.js'));
   });
 
   it('source map', () => {
-    let map = '//# sourceMappingURL=data:application/json;base64';
-    let generated = transform(file('original.js'), {maps: true});
+    let generated = transform(file('original.js'), {maps: true}).map;
 
-    assert.include(generated, map);
+    assert.deepEqual(generated, file('map.json'));
   });
 });
